1dab40858856a8ce3885ea77721914d8a7dd26be
Ivan Ukhov <ivan.ukhov.work@gmail.com>
2024-02-08 16:31:08 +0600

    tasks 3 and 3* done

--- src/classes.py
+++ src/classes.py
@@ -2,7 +2,7 @@ class Category:
     """"Этот класс содержит в себе категории продуктов"""
     name: str
     description: str
-    products: list
+    __products: list
     total_categories_quantity = 0
     unique_products_quantity = 0
 
@@ -13,7 +13,7 @@ class Category:
         Category.total_categories_quantity += 1
         Category.unique_products_quantity += len(products)
 
-    def get_products(self): # геттер
+    def get_products(self):  # геттер
         return [f'{product.name}, {product.price} руб. Остаток: {product.quantity} шт.' for product in self.__products]
 
 
@@ -29,3 +29,15 @@ class Product:
         self.description = description
         self.price = price
         self.quantity = quantity
+
+    @staticmethod
+    def make_product(name, description, price, quantity, products_list=None):
+        if products_list is None:
+            return Product(name, description, price, quantity)
+        else:
+            for product in products_list:
+                if product.name == name:
+                    new_quantity = int(product.quantity) + quantity
+                    new_price = max(price, product.price)
+                    return Product(name, description, new_price, new_quantity)
+            return Product(name, description, price, quantity)
